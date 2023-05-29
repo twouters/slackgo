04e062c05e8b81d16b77b105276cea2c0bb9553c
Max Sandholm <vurpo@vurpo.fi>
2023-05-29 14:20:42 +0300

    Add file_access field on file object

--- files.go
+++ files.go
@@ -89,6 +89,7 @@ type File struct {
 	NumStars        int      `json:"num_stars"`
 	IsStarred       bool     `json:"is_starred"`
 	Shares          Share    `json:"shares"`
+	FileAccess      string   `json:"file_access"`
 }
 
 type Share struct {
